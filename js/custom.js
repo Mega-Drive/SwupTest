a5e6ce7bd3255d65fca9144e6fa4c443bc155a28
Mega Drive <Mega-Drive@users.noreply.github.com>
2019-05-15 15:33:10 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	
+
+});
